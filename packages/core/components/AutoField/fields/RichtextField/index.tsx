b10b17db4f41520ddd96746641d8a770630528b1
Rami Bitar <rbitar@gmail.com>
2026-04-20 21:33:08 -0400

    initial commit

---
+++ packages/core/components/AutoField/fields/RichtextField/index.tsx
@@ -0,0 +1,48 @@
+import { lazy, Suspense } from "react";
+import { FieldPropsInternal } from "../..";
+import { RichtextField as RichtextFieldType } from "../../../../types";
+import { EditorFallback } from "../../../RichTextEditor/components/EditorFallback";
+import { useDeepField } from "../../lib/use-deep-field";
+
+const Editor = lazy(() =>
+  import("../../../RichTextEditor/components/Editor").then((m) => ({
+    default: m.Editor,
+  }))
+);
+
+export const RichtextField = ({
+  onChange,
+  readOnly = false,
+  id,
+  name = id,
+  label,
+  labelIcon,
+  Label,
+  field,
+}: FieldPropsInternal) => {
+  const content = useDeepField(name);
+
+  const editorProps = {
+    onChange: onChange,
+    content,
+    readOnly: readOnly,
+    field: field as RichtextFieldType,
+    id: id,
+    name: name,
+  };
+
+  return (
+    <>
+      <Label
+        label={label || name}
+        icon={labelIcon}
+        readOnly={readOnly}
+        el="div"
+      >
+        <Suspense fallback={<EditorFallback {...editorProps} />}>
+          <Editor {...editorProps} />
+        </Suspense>
+      </Label>
+    </>
+  );
+};
---
+++ packages/core/components/AutoField/fields/TextareaField/index.tsx
@@ -0,0 +1,40 @@
+import getClassNameFactory from "../../../../lib/get-class-name-factory";
+import styles from "../../styles.module.css";
+import { FieldPropsInternal } from "../..";
+import { useLocalValue } from "../../lib/use-local-value";
+
+const getClassName = getClassNameFactory("Input", styles);
+
+export const TextareaField = ({
+  field,
+  onChange,
+  readOnly,
+  id,
+  name = id,
+  label,
+  labelIcon,
+  Label,
+}: FieldPropsInternal) => {
+  const [localValue, onChangeLocal] = useLocalValue(name, onChange);
+
+  return (
+    <Label
+      label={label || name}
+      icon={labelIcon}
+      readOnly={readOnly}
+    >
+      <textarea
+        id={id}
+        className={getClassName("input")}
+        autoComplete="off"
+        name={name}
+        value={typeof localValue === "undefined" ? "" : localValue}
+        onChange={(e) => onChangeLocal(e.currentTarget.value)}
+        readOnly={readOnly}
+        tabIndex={readOnly ? -1 : undefined}
+        rows={5}
+        placeholder={field.type === "textarea" ? field.placeholder : undefined}
+      />
+    </Label>
+  );
+};
